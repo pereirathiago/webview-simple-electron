1625ff09d0c1a1ed03a6b951502ed726da98f1ac
ThiagoPereira232 <pereira.tp.thiago@gmail.com>
2020-11-19 19:40:59 -0300

    links

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
 module.exports = {
     url: "https://youtube.com/",
     url2: "https://www.google.com/",
-    //server: "http://localhost:5500/",
-	server: "http://127.0.0.1/pass4u.ml/gerar.php/",
+    server: "http://localhost:5500/",
     width: 600,
     height: 400
 }
