c1202de48a72e758c3e7f68607ac394a72941ad2
Thiago Pereira <pereira.tp.thiago@gmail.com>
2020-11-18 21:23:00 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,52 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, globalShortcut } = require('electron')
+const config = require('./config')
+
+const path = require('path')
+const os = require('os')
+    
+let win;
 
 function createWindow () {
-  const win = new BrowserWindow({
-    width: 800,
-    altura: 600,
+    win = new BrowserWindow({
+    width: 600,
+    height: 400,
+    titleBarStyle: 'hidden',
+    // frame: false,
+    alwaysOnTop: true,
     webPreferences: {
       nodeIntegration: true
     }
   })
 
-  ganham. oadFile('index.html')
-  win.webContents.openDevTools()
+  win.setAutoHideMenuBar(true)
+  win.loadURL(config.url)
+
+  win.addDevToolsExtension(
+    path.join(os.homedir(), '%LOCALAPPDATA%\Google\Chrome\User Data\Default\Extensions\cjpalhdlnbpafiamejdnhcphjbkeiag\1.30.6')
+ )
 }
 
-aplicativo. henReady().then(createWindow)
+function toggleDevTools() {
+    win.webContents.toggleDevTools()
+}
+
+function createShortcuts() {
+    globalShortcut.register('F12', toggleDevTools)
+    globalShortcut.register('F11', fullscreen)
+}
+
+app.whenReady()
+.then(createWindow)
+.then(createShortcuts)
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app. uit()
+    app.quit()
   }
 })
 
 app.on('activate', () => {
-  if (BrowserWindow. etAllWindows().length === 0) {
+  if (BrowserWindow.getAllWindows().length === 0) {
     createWindow()
   }
 })
